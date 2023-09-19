0848bd3129ec9c3d1cf8c2c483d45e2d36a8c3a6
e-gotesjo <120645559+gotesjo@users.noreply.github.com>
2023-09-19 11:37:05 +0200

    fungerande popup-contact modal

--- script.js
+++ script.js
@@ -84,14 +84,25 @@ validateForm = () => {
 }
 
 
+clearFormError = () => {
+    messageError.innerHTML = '';
+    teleError.innerHTML = '';
+    nameError.innerHTML = '';
+    emailError.innerHTML = '';
+
+}
+
+//Hämta formuläret
+let form = document.getElementById("contactform");
+
 // Get the modal
-var modal = document.getElementById("myModal");
+let modal = document.getElementById("contact-wrapper");
 
 // Get the button that opens the modal
-var btn = document.getElementById("support-button");
+let btn = document.getElementById("support-button");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
@@ -101,6 +112,8 @@ btn.onclick = function() {
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  form.reset();
+  clearFormError();
 }
 
 // When the user clicks anywhere outside of the modal, close it
@@ -109,4 +122,3 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
-3
